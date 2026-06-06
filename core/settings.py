bcf4d2c467971d84c46603221f7dc9f4dfe915cd
KellySimiyu <kellysimiyu122@gmail.com>
2026-06-06 23:27:18 +0300

    fixed missing dj_database_url import in settings.py

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +76,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
+
+# Demo hardcoded Postgres linkage configuration
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        'postgresql://ai_jobboard_db_user:pzq8TIHkL11gXb6bWRjH1jfNpL4tEDsf@dpg-d8i812flk1mc73fspvl0-a.oregon-postgres.render.com/ai_jobboard_db'
+    )
 }
 
 
