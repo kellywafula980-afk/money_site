5cafe047049c472447651228b8f8acc23864d602
KellySimiyu <kellysimiyu122@gmail.com>
2026-06-08 14:00:08 +0300

    Monetization: Corrected ads.txt template path mapping

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView  # 👈 MAKE SURE THIS IMPORT IS HERE
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('jobs.urls')), # This routes everything else to your jobs app
+    path('admin/', admin.site.admin_site_urls), # or your standard admin path
+    path('', include('jobs.urls')),
+    
+    # 💰 PASTE THIS MAGIC LINE RIGHT HERE FOR GOOGLE
+    path('ads.txt', TemplateView.as_view(template_name='ads.txt', content_type='text/plain')),
+    
 ]
