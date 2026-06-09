ec924ae38b8d734eb60ad9733b11736baf9a097b
KellySimiyu <kellysimiyu122@gmail.com>
2026-06-09 13:17:47 +0300

    chore: add Google Search Console verification route and template

--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     
     # This maps perfectly to jobs/templates/jobs/ads.txt
     path('ads.txt', TemplateView.as_view(template_name='jobs/ads.txt', content_type='text/plain')),
+    
+    # 🚀 ADD THIS NEW LINE FOR GOOGLE VERIFICATION:
+    path('google56bce93523ece129.html', TemplateView.as_view(template_name='jobs/google56bce93523ece129.html', content_type='text/html')),
 ]
