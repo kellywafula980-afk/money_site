63191f66d79f7a0ee2853a03710878491a2d1e86
KellySimiyu <kellysimiyu122@gmail.com>
2026-07-26 16:16:01 +0300

    added the correct domain

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ if not SECRET_KEY:
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # Hosts allowed to serve the application
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'globalgigs-0096.onrender.com').split(',')
 
 # ========== APPLICATION DEFINITION ==========
 INSTALLED_APPS = [
